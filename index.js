7a01a72051493417899d17b0b9c31d50eace9684
Anna Moura Sobral <annamourasobral@gmail.com>
2022-05-20 12:45:42 +0200

    functionality

---
+++ index.js
@@ -0,0 +1,33 @@
+var password1 =document.getElementById("password1");
+var password2 =document.getElementById("password1");
+var password3 =document.getElementById("password1");
+var password4 =document.getElementById("password1");
+
+function generate() {
+    var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var passwordLength = 12;
+    var password1 = "";
+    var password2 = "";
+    var password3 = "";
+    var password4 = "";
+    for (var i = 0; i < passwordLength; i++) {
+        var randomNumber = Math.floor(Math.random() * chars.length);
+        password1 += chars.substring(randomNumber, randomNumber +1);
+    }
+    for (var i = 0; i < passwordLength; i++) {
+        var randomNumber = Math.floor(Math.random() * chars.length);
+        password2 += chars.substring(randomNumber, randomNumber +1);
+    }
+    for (var i = 0; i < passwordLength; i++) {
+        var randomNumber = Math.floor(Math.random() * chars.length);
+        password3 += chars.substring(randomNumber, randomNumber +1);
+    }
+    for (var i = 0; i < passwordLength; i++) {
+        var randomNumber = Math.floor(Math.random() * chars.length);
+        password4 += chars.substring(randomNumber, randomNumber +1);
+    }
+    document.getElementById("password1").value = password1;
+    document.getElementById("password2").value = password2;
+    document.getElementById("password3").value = password3;
+    document.getElementById("password4").value = password4;
+}
